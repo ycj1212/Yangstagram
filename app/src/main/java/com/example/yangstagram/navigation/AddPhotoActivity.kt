d056ee0aa3884cff9d7c0174d38b34d9bb7b91c6
Yang Cheol Joo <rlaengks0001@naver.com>
2020-12-31 15:22:21 +0900

    Add feature to content data model

--- app/src/main/java/com/example/yangstagram/navigation/AddPhotoActivity.kt
+++ app/src/main/java/com/example/yangstagram/navigation/AddPhotoActivity.kt
@@ -6,10 +6,16 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import com.example.yangstagram.R
+import com.example.yangstagram.navigation.model.ContentDTO
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -20,14 +26,22 @@ class AddPhotoActivity : AppCompatActivity() {
     lateinit var photoUri: Uri
     lateinit var photoImage: ImageView
 
+    lateinit var editExplain: EditText
+
+    lateinit var auth: FirebaseAuth
+    lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_photo)
 
-        photoImage = findViewById(R.id.addphoto_image)
-
-        // Initiate storage
+        // Initiate
         storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        photoImage = findViewById(R.id.addphoto_image)
+        editExplain = findViewById(R.id.addphoto_edit_explain)
 
         // Open the album
         val photoPickerIntent = Intent(Intent.ACTION_PICK)
@@ -63,10 +77,63 @@ class AddPhotoActivity : AppCompatActivity() {
 
         val storageRef = storage.reference.child("images").child(imageFileName)
 
-        // File Upload
-        storageRef.putFile(photoUri).addOnSuccessListener {
-            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        // Promise method (Google recommended)
+        storageRef.putFile(photoUri).continueWithTask { task: Task<UploadTask.TaskSnapshot> ->
+            return@continueWithTask storageRef.downloadUrl
+        }.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            // Insert downloadUrl of image
+            contentDTO.imageUrl = uri.toString()
+
+            // Insert uid of user
+            contentDTO.uid = auth.currentUser?.uid
+
+            // Insert userId
+            contentDTO.userId = auth.currentUser?.email
+
+            // Insert explain of content
+            contentDTO.explain = editExplain.text.toString()
+
+            // Insert timestamp
+            contentDTO.timestamp = System.currentTimeMillis()
+
+            firestore.collection("images").document().set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
             finish()
         }
+
+        // Callback method
+        /*
+        storageRef.putFile(photoUri).addOnSuccessListener {
+            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+            storageRef.downloadUrl.addOnSuccessListener { uri ->
+                var contentDTO = ContentDTO()
+
+                // Insert downloadUrl of image
+                contentDTO.imageUrl = uri.toString()
+
+                // Insert uid of user
+                contentDTO.uid = auth.currentUser?.uid
+
+                // Insert userId
+                contentDTO.userId = auth.currentUser?.email
+
+                // Insert explain of content
+                contentDTO.explain = editExplain.text.toString()
+
+                // Insert timestamp
+                contentDTO.timestamp = System.currentTimeMillis()
+
+                firestore.collection("images").document().set(contentDTO)
+
+                setResult(Activity.RESULT_OK)
+
+                finish()
+            }
+        }
+        */
     }
 }
