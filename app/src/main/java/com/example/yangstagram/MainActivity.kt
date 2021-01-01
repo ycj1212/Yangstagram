0c2141d937a0374052c160badc6e148440e6ba36
Yang Cheol Joo <rlaengks0001@naver.com>
2021-01-01 22:07:39 +0900

    Add feature to user page

--- app/src/main/java/com/example/yangstagram/MainActivity.kt
+++ app/src/main/java/com/example/yangstagram/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.yangstagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -51,6 +52,11 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 val userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
                 return true
             }
--- app/src/main/java/com/example/yangstagram/navigation/UserFragment.kt
+++ app/src/main/java/com/example/yangstagram/navigation/UserFragment.kt
@@ -4,14 +4,75 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.yangstagram.R
+import com.example.yangstagram.navigation.model.ContentDTO
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UserFragment : Fragment() {
+    lateinit var fragmentView: View
+    lateinit var firestore: FirebaseFirestore
+    lateinit var auth: FirebaseAuth
+    var uid: String? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
+        fragmentView = LayoutInflater.from(activity).inflate(R.layout.fragment_user, container, false)
 
-        return view
+        uid = arguments?.getString("destinationUid")
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        val recyclerView = fragmentView.findViewById<RecyclerView>(R.id.account_recyclerview).apply {
+            layoutManager = GridLayoutManager(activity, 3)
+            adapter = UserFragmentRecyclerViewAdapter()
+        }
+
+        return fragmentView
+    }
+
+    inner class UserFragmentRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        val contentDTOs: ArrayList<ContentDTO> = arrayListOf()
+
+        init {
+            firestore.collection("images").whereEqualTo("uid", uid).addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                // Sometimes, This code return null of querySnapshot when it signout
+                if (querySnapshot == null) return@addSnapshotListener
+
+                // Get data
+                for (snapshot in querySnapshot.documents) {
+                    contentDTOs.add(snapshot.toObject(ContentDTO::class.java)!!)
+                }
+
+                fragmentView.findViewById<TextView>(R.id.account_tv_post_count).text = contentDTOs.size.toString()
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val width = resources.displayMetrics.widthPixels / 3
+            val imageview = ImageView(parent.context)
+            
+            imageview.layoutParams = LinearLayoutCompat.LayoutParams(width, width)
+            return CustomViewHolder(imageview)
+        }
+
+        inner class CustomViewHolder(var imageview: ImageView) : RecyclerView.ViewHolder(imageview) {
+
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val imageview = (holder as CustomViewHolder).imageview
+            Glide.with(holder.itemView.context).load(contentDTOs[position].imageUrl).apply(RequestOptions().centerCrop()).into(imageview)
+        }
+
+        override fun getItemCount(): Int = contentDTOs.size
     }
 }
